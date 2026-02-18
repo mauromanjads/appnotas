40edf71b467621a63c4c07752caf70e3ab943166
MauricioManjarres <mauricio.manjarresd@pca.edu.co>
2026-02-18 13:31:55 -0500

    ajustando backend

--- app/backend/main.py
+++ app/backend/main.py
@@ -3,27 +3,29 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .database import Base, engine
 from . import models
-from .routers import marcas, notas
-from .routers import login
-
+from .routers import marcas, notas, login
 
 app = FastAPI()
 
-# 🔹 CONFIGURACIÓN CORS (IMPORTANTE)
+# 🔹 CONFIGURACIÓN CORS
+# Solo permitimos los dominios que usarán tu frontend
+origins = [
+    "https://appnotas-5ai7uxful-mauricio-manjarres-projects.vercel.app",  # Frontend Vercel
+    "http://localhost:5173",  # Frontend local para desarrollo
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Para entrevista/demo
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=origins,       # dominios permitidos
+    allow_credentials=True,      # cookies, headers de autenticación
+    allow_methods=["*"],         # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],         # cabeceras personalizadas
 )
 
-# Crear tablas
+# 🔹 Crear tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
-
-# Incluir routers
+# 🔹 Incluir routers
 app.include_router(marcas.router)
 app.include_router(notas.router)
 app.include_router(login.router)
-
